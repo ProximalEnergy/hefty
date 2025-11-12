240adba1099fc54b77cb807cec454b1db99e36ce
Marcus Marosvari <65305139+marcus-maro@users.noreply.github.com>
2025-11-12 10:09:59 -0700

    [web-app] Refresh Custom Dashboards Menu UI (#272)
    * Refresh Custom Dashboards Menu UI
    
    * Improve handling of disabled project state

--- web-app/src/pages/projects/custom_dash/CustomDashMenu.tsx
+++ web-app/src/pages/projects/custom_dash/CustomDashMenu.tsx
@@ -3,25 +3,37 @@ import {
   useGetUserDashboards,
 } from '@/api/v1/operational/project/custom_dash'
 import { useSelectProject } from '@/api/v1/operational/projects'
+import { NoData, PageError } from '@/components/Error'
 import { PageLoader } from '@/components/Loading'
+import { PageTitle } from '@/components/PageTitle'
 import {
   ActionIcon,
   Button,
+  Divider,
   Group,
   Modal,
   Stack,
   Text,
-  Title,
   Tooltip,
 } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { IconCopy, IconPlus, IconShare, IconTrash } from '@tabler/icons-react'
 import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router'
+import { Link, useParams } from 'react-router'
+
+const ACTION_ICON_PROPS = {
+  size: 'lg',
+  variant: 'light',
+}
+
+const ICON_PROPS = {
+  width: '70%',
+  height: '70%',
+  stroke: 1.5,
+}
 
 const CustomDashMenu = () => {
   const { projectId } = useParams<{ projectId: string }>()
-  const navigate = useNavigate()
   const [opened, { open, close }] = useDisclosure(false)
   const [dashboardToDelete, setDashboardToDelete] = useState<{
     id: string
@@ -69,73 +81,108 @@ const CustomDashMenu = () => {
     return <PageLoader />
   }
 
+  if (userDashboards.error) {
+    return <PageError error={userDashboards.error} />
+  }
+  if (project.error) {
+    return <PageError error={project.error} />
+  }
+
+  if (!userDashboards.data || !project.data) {
+    return <NoData />
+  }
+
   const disabledProjects = ['sun_streams_3', 'lancaster', 'snipesville_2']
   const isDisabled =
     project.data && disabledProjects.includes(project.data.name_short)
 
   return (
     <Stack p="md" h="100%">
-      <Title>Custom Dashboards</Title>
-      {userDashboards.data?.map((dashboard) => (
-        <Group
-          key={dashboard.dashboard_id}
-          justify="space-between"
-          align="center"
-        >
-          <Button onClick={() => navigate(dashboard.dashboard_id)} flex={1}>
-            {dashboard.dashboard_name}
-          </Button>
+      <Group justify="space-between">
+        <PageTitle>Custom Dashboards</PageTitle>
 
-          <Group>
-            <Tooltip label="Delete">
-              <ActionIcon
-                variant="light"
-                color="red"
-                onClick={() =>
-                  handleDeleteClick(
-                    dashboard.dashboard_id,
-                    dashboard.dashboard_name,
-                  )
-                }
-                loading={deletingId === dashboard.dashboard_id} // only this one spins
-                disabled={
-                  Boolean(deletingId) && deletingId !== dashboard.dashboard_id
-                } // optional: lock others
+        {/* If the project is not disabled, show the new dashboard button */}
+        {!isDisabled && (
+          <Link to={`/projects/${projectId}/custom-dash/new`}>
+            <Button
+              style={{ cursor: isDisabled ? 'not-allowed' : 'pointer' }}
+              leftSection={<IconPlus size={14} />}
+            >
+              New Dashboard
+            </Button>
+          </Link>
+        )}
+      </Group>
+
+      {isDisabled ? (
+        // If the project is disabled, show the disabled message
+        <Text>
+          Custom dashboards are not enabled for this project. Please request
+          custom dashboards using the feedback button in the bottom left hand
+          corner of the screen.
+        </Text>
+      ) : userDashboards.data.length === 0 ? (
+        // If the project is not disabled and the user has no dashboards, show the no dashboards message
+        <Text>
+          You don&apos;t have any Custom Dashboards yet. Click the New Dashboard
+          button above to get started!
+        </Text>
+      ) : (
+        // If the project is not disabled and the user has dashboards, show the dashboards
+        [...userDashboards.data]
+          .sort((a, b) => a.dashboard_name.localeCompare(b.dashboard_name))
+          .map((dashboard) => (
+            <Group
+              key={dashboard.dashboard_id}
+              justify="space-between"
+              align="center"
+            >
+              <Link
+                to={`/projects/${projectId}/custom-dash/${dashboard.dashboard_id}`}
+                style={{ color: 'inherit' }}
               >
-                <IconTrash size={16} />
-              </ActionIcon>
-            </Tooltip>
+                <Text size="lg">{dashboard.dashboard_name}</Text>
+              </Link>
+              <Divider variant="dashed" flex={1} />
 
-            <Tooltip label="Share coming soon!">
-              <ActionIcon variant="light" color="blue" disabled>
-                <IconShare size={16} />
-              </ActionIcon>
-            </Tooltip>
+              {/* Delete dashboard */}
+              <Tooltip label="Delete">
+                <ActionIcon
+                  {...ACTION_ICON_PROPS}
+                  color="red"
+                  onClick={() =>
+                    handleDeleteClick(
+                      dashboard.dashboard_id,
+                      dashboard.dashboard_name,
+                    )
+                  }
+                  loading={deletingId === dashboard.dashboard_id}
+                  disabled={
+                    Boolean(deletingId) && deletingId !== dashboard.dashboard_id
+                  }
+                >
+                  <IconTrash {...ICON_PROPS} />
+                </ActionIcon>
+              </Tooltip>
 
-            <Tooltip label="Duplicate coming soon!">
-              <ActionIcon variant="light" color="gray" disabled>
-                <IconCopy size={16} />
-              </ActionIcon>
-            </Tooltip>
-          </Group>
-        </Group>
-      ))}
-      <Tooltip
-        label="Custom dashboards are not enabled for this project. Please request custom dashboards using the feedback button in the bottom left hand corner of the screen."
-        disabled={!isDisabled}
-        multiline
-        w={220}
-        withArrow
-      >
-        <Button
-          onClick={() => navigate('new')}
-          disabled={isDisabled}
-          style={{ cursor: isDisabled ? 'not-allowed' : 'pointer' }}
-        >
-          <IconPlus /> New Dashboard
-        </Button>
-      </Tooltip>
+              {/* Share dashboard */}
+              <Tooltip label="Share coming soon!">
+                <ActionIcon {...ACTION_ICON_PROPS} color="blue" disabled>
+                  <IconShare {...ICON_PROPS} />
+                </ActionIcon>
+              </Tooltip>
 
+              {/* Duplicate dashboard */}
+              <Tooltip label="Duplicate coming soon!">
+                <ActionIcon {...ACTION_ICON_PROPS} color="gray" disabled>
+                  <IconCopy {...ICON_PROPS} />
+                </ActionIcon>
+              </Tooltip>
+            </Group>
+          ))
+      )}
+
+      {/* Delete Dashboard Modal */}
       <Modal opened={opened} onClose={close} title="Delete Dashboard" centered>
         <Stack>
           <Text>
