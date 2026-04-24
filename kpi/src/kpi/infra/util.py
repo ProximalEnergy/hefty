2ee147c66d71574500dc6764190776486e0bbe7f
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2026-04-24 09:41:52 -0700

    [kpi] Deduplicate pandas datetime numpy helpers in kpi (#1369)

--- kpi/src/kpi/infra/util.py
+++ kpi/src/kpi/infra/util.py
@@ -1,6 +1,6 @@
-import pandas as pd
 from core.crud.operational.projects import get_projects
 from core.db_query import OutputType
+from core.utils.pandas_datetime import index_to_numpy_ns, series_to_numpy_ns
 
 from core import models
 
@@ -14,41 +14,3 @@ def get_project_from_database(name_short: str) -> models.Project:
         )
     project = projects[0]
     return project
-
-
-def index_to_numpy_ns(*, index: pd.Index):
-    """Convert index to numpy, coercing datetimes to datetime64[ns].
-
-    Args:
-        index: Index to convert.
-    """
-    if isinstance(index, pd.DatetimeIndex):
-        if index.tz is not None:
-            return (
-                index.tz_convert("UTC")
-                .tz_localize(None)
-                .to_numpy(dtype="datetime64[ns]")
-            )
-        return index.to_numpy(dtype="datetime64[ns]")
-    return index.to_numpy()
-
-
-def series_to_numpy_ns(*, series: pd.Series):
-    """Convert series to numpy, coercing datetimes to datetime64[ns].
-
-    Args:
-        series: Series to convert.
-    """
-    if isinstance(series.dtype, pd.DatetimeTZDtype):
-        return (
-            series.dt.tz_convert("UTC")
-            .dt.tz_localize(None)
-            .to_numpy(dtype="datetime64[ns]")
-        )
-
-    if pd.api.types.is_datetime64_any_dtype(series.dtype):
-        if isinstance(series.dtype, pd.DatetimeTZDtype):
-            series = series.dt.tz_convert("UTC").dt.tz_localize(None)
-        return series.to_numpy(dtype="datetime64[ns]")
-
-    return series.to_numpy()
