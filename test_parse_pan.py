6dfcaf15f71997ad90c3575f562097146836381a
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2026-05-04 12:24:24 -0700

    [root] code cleanup (#1481)

--- test_parse_pan.py
+++
@@ -1,8 +0,0 @@
-import sys
-from api.app.domain.equipment.pv_module.parse_pan.c_parse_pan import parse_pan
-
-with open("FS-6430A CdTe Dec2017_v640.PAN", "rb") as f:
-    content = f.read()
-
-res = parse_pan(file_content=content)
-print(res)
